6bdafae24295cc49429f3ff551bcf09027a415a4
Demet Çalışkan <demet@aposto.com>
2023-04-04 05:24:11 +0300

    add result detail component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,29 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, results, errorMsg] = useResults()
 
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price
+    })
+  }
+
   return (
-    <View style={styles.backgroundStyle}>
-      {errorMsg?.length > 1 && <Text>{errorMsg}</Text>}
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
-      <Text>We have found {results.length} results</Text>
-    </View>
+      {errorMsg?.length > 1 && <Text>{errorMsg}</Text>}
+      <ScrollView>
+        <ResultsList
+          title='Cost Effective'
+          results={filterResultsByPrice('$')}
+        />
+        <ResultsList title='Bit Pricier' results={filterResultsByPrice('$$')} />
+        <ResultsList
+          title='Big Spender'
+          results={filterResultsByPrice('$$$')}
+        />
+      </ScrollView>
+    </>
   )
 }
 
-const styles = StyleSheet.create({
-  backgroundStyle: {
-    backgroundColor: '#fff',
-  },
-})
+const styles = StyleSheet.create({})
 
 export default SearchScreen
